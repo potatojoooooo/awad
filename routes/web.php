ebfa82cd5675c70ed0c60272ffb127d7bbb3a30c
LAM CHUN HONG <chunhonglam821591@gmail.com>
2023-04-08 23:48:19 +0800

    change on login and register blade

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +19,25 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ServiceController;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Auth::routes();
-Route::get('/login/admin', [LoginController::class, 'showAdminLoginForm']);
+Route::get('/login/admin', [LoginController::class, 'showAdminLoginForm'])->name('login.admin');
+Route::get('/login/user', [LoginController::class, 'showUserLoginForm'])->name('login.user');
 Route::get('/register/admin', [RegisterController::class, 'showAdminRegisterForm']);
+Route::get('/register/user', [RegisterController::class, 'showUserRegisterForm']);
 Route::post('/login/admin', [LoginController::class, 'adminLogin']);
+Route::post('/login/user', [LoginController::class, 'userLogin']);
 Route::post('/register/admin', [RegisterController::class, 'createAdmin']);
+Route::post('/register/user', [RegisterController::class, 'createUser']);
 Route::group(['middleware' => 'auth:admin'], function () {
     Route::view('/admin', 'admin');
+});
+
+Route::group(['middleware' => 'auth:user'], function () {
+    Route::view('/user', 'user');
 });
 
 Route::view("displayBooking","booking.displayBooking");
@@ -35,5 +46,4 @@ Route::view("contactus","contactUs");
 Route::view("user","user");
 Route::get("services",[ServiceController::class, 'index']);
 Route::get('logout', [LoginController::class, 'logout']);
-Route::view("/","home");
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
