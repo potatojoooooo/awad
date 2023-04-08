801ec9431b920473f2a72213ef87141ebb25a75d
chenashe <shinyee626@gmail.com>
2023-04-09 03:09:51 +0800

    update BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 use App\Models\Booking;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class BookingController extends Controller
 {
@@ -18,4 +18,82 @@ class BookingController extends Controller
             ->get();
         return view('booking.displayBooking', ['bookings' => $bookings]);
     }
+
+    public function showUpdate($id)
+    {   
+        $booking = Booking::find($id);
+        $dateOnly = new Carbon($booking->date);
+        $booking->date = $dateOnly->toDateString(); 
+        $booking->time = Carbon::parse($booking->time)->format('H:i');
+        return view("booking.updateBooking", ['booking'=>$booking]);
+    }
+
+    public function updateBooking(Request $req)
+    {
+        $req->validate([
+            'date' => 'required|after:today',
+            //'time' => ['required', 'after:' . Carbon::now()->format('H:i'),],
+            'time' => 'required|after:08:59|before:17:01',
+            'serviceID' => 'required',      
+        ], [
+            'date.after' => 'The new date must be tommorrow or a future date.',
+        ]);
+
+        $booking = Booking::find($req->id);
+        
+        // Combine the date and time into a datetime object
+        $datetime = Carbon::createFromFormat('Y-m-d H:i:s', $req->date.' '.$req->time.':00');
+        $booking->date = $datetime;
+        $booking->time = $datetime;
+        $booking->serviceID = $req->serviceID;
+        $booking->save();
+        return redirect("displayBooking");
+    }
+    // public function processForm(Request $request)
+    // {
+    //     $selectedOptions = $request->input('selectedOptions');
+    //     // return view('form', ['selectedOptions' => $selectedOptions]);
+    //     
+    // }
+
+    // protected function validateBooking(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'date' => 'required',
+    //         'time' => 'required',
+    //         'serviceID' => 'required',
+    //         'name' => 'required|max:2',
+    //         'phone' => 'required| max:2'
+    //     ]);
+    // }
+
+    protected function validateBooking(array $data)
+    {
+        return Validator::make($data, [
+            'date' => 'required | date',
+            'time' => 'required',
+            'serviceID' => 'required | integer',
+            'name' => 'required|max:2',
+            'phone' => 'required|max:2'
+        ], [
+            'name.required' => 'Name is required.',
+            'phone.required' => 'Phone is required.'
+        ]);
+    }
+
+    protected function createBooking(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        Booking::create([
+            'date' => $request->date,
+            'time' => $request->time,
+            'serviceID' => $request->serviceID,
+            'name' => $request->name,
+            'phone' => $request->phone,
+
+        ]);
+        return redirect()->intended('login/admin');
+    }
+
+  
 }
